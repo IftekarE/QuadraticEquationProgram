d6104588e5047f8976de26aea2eb1f7422fcae22
Iftekar emon <3m0n1ft3kar@gmail.com>
2020-02-10 08:27:38 -0500

    added java script

---
+++ logic.js
@@ -0,0 +1,20 @@
+var hold1 = 0;
+var hold2 = 0;
+var hold3 = 0;
+var hold4 = 0;
+var temp1 = 0;
+var temp2 = 0;
+var temp3 = 0;
+var a = 0;
+var b = 0;
+var c = 0;
+var vertex = 0;
+var AOS = 0;
+var num_of_roots = 0;
+var type_of_root = 0;
+var root1 = 0;
+var root2 = 0;
+
+function checks(){
+    
+}

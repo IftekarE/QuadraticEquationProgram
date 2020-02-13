b8a189cafbcec7808fd796230a4aa4247878df8d
Iftekar emon <3m0n1ft3kar@gmail.com>
2020-02-13 08:45:28 -0500

    the program now can solve for veertex and aos

--- logic.js
+++ logic.js
@@ -15,6 +15,10 @@ var num_of_roots = 0;
 var type_of_root = 0;
 var root1 = 0;
 var root2 = 0;
+var x = 0;
+var y = 0;
+
+
 //Variables initlazing END 
 
 //Clearing variables START
@@ -35,6 +39,23 @@ function clear(){//this function is setting all variables to 0
     type_of_root = 0;
     root1 = 0;
     root2 = 0; 
+    x = 0;
+    y = 0;
+    
+    meshold0 = 0;
+    meshold1 = 0;
+    meshold2 = 0;
+    meshold3 = 0;
+    meshold4 = 0;
+    meshold5 = 0;
+    meshold6 = 0;
+    idhold0 = 0;
+    idhold1 = 0;
+    idhold2 = 0;
+    idhold3 = 0;
+    idhold4 = 0;
+    idhold5 = 0;
+    idhold6 = 0;
 }
 //Clearing variables END
 
@@ -58,11 +79,11 @@ function Acheck(inputtxt){
                 if(temp1 % 2 == 0){//modding input by 2 to see if the input is a whole number
                     a = temp1;
                 }else{
-                    if(temp1 % 2 == 1){
+                    if(temp1 % 2 == 1){//modding input by 2 to see if the input is a whole number
                         a = temp1;
                     }else{
                         meshold0 = "The input can only be a whole number";
-                        notifications();
+                        notifications();//calling caller block 
                     }
                 }
             }
@@ -71,28 +92,28 @@ function Acheck(inputtxt){
 }
 function Bcheck(inputtxt){
     temp1 = inputtxt.value;
-    if(inputtxt.value.length == 0){
+    if(inputtxt.value.length == 0){//checking if the input is blank  
         meshold0 = "The B input cannot be blank";
         notifications();
     }else{
-        if(temp1 == 0){
+        if(temp1 == 0){//checking if the B input is blank
             b = temp1;
             meshold0 = ("b is " + b);
             notifications();
         }else{
-            var letters = /^[A-Za-z]+$/;
+            var letters = /^[A-Za-z]+$/;//chicking to see if the input is a letter 
             if(inputtxt.value.match(letters)){
                 meshold0 = "The input can only be a negative or positive integer";
                 notifications();
             }else{
-                if(temp1 % 2 == 0){
+                if(temp1 % 2 == 0){//modding input by 2 to see if the input is a whole number
                     b = temp1;
                 }else{
-                    if(temp1 % 2 == 1){
+                    if(temp1 % 2 == 1){//modding input by 2 to see if the input is a whole number
                         b = temp1;
                     }else{
                         meshold0 = "The input can only be a whole number";
-                        notifications();
+                        notifications();//calling caller block 
                     }
                 }
             }
@@ -101,28 +122,30 @@ function Bcheck(inputtxt){
 }
 function Ccheck(inputtxt){
     temp1 = inputtxt.value;
-    if(inputtxt.value.length == 0){
+    if(inputtxt.value.length == 0){//checking if the input is blank  
         meshold0 = "The C input cannot be blank";
         notifications();
     }else{
-        if(temp1 == 0){
+        if(temp1 == 0){//checking if the C input is blank
             c = temp1;
             meshold0 = "c is " + c;
             notifications();
         }else{
-            var letters = /^[A-Za-z]+$/;
+            var letters = /^[A-Za-z]+$/;//chicking to see if the input is a letter 
             if(inputtxt.value.match(letters)){
                 meshold0 = "The input can only be a negative or positive integer";
                 notifications();
             }else{
-                if(temp1 % 2 == 0){
+                if(temp1 % 2 == 0){//modding input by 2 to see if the input is a whole number
                     c = temp1;
+                    veraos();
                 }else{
-                    if(temp1 % 2 == 1){
+                    if(temp1 % 2 == 1){//modding input by 2 to see if the input is a whole number
                         c = temp1;
+                        veraos();
                     }else{
                         meshold0 = "The input can only be a whole number";
-                        notifications();
+                        notifications();//calling caller block 
                     }
                 }
             }
@@ -132,7 +155,7 @@ function Ccheck(inputtxt){
 //Checking for input END
 
 //setting the resutlts to the message board START 
-function setto(){
+function setto(){//this code segment sets the message to the message boards 
     meshold1 = vertex;
     meshold2 = AOS;
     meshold3 = num_of_roots;
@@ -142,3 +165,20 @@ function setto(){
     results();
 }
 //setting the resutlts to the message board END 
+
+//vertex and AOS flowchart START
+function veraos(){//this program is finding the vertex 
+    hold1 = b * -1;//multipling b by -1 
+    hold2 = a * 2;// multiplying a by 2 
+    x = hold1 / hold2;//this formula represents b * -1 / 2a
+    AOS = x;// the x value of the vertex is the AOS 
+    temp1 = x * x;//x is multiplying by x to represent x^2
+    temp1 = a * temp1;//x * temp1 representing ax^2
+    temp2 = b * x;//representing b*x
+    y = temp1 + temp2 + c;//this represents my formula y = ax^2 + bx + c
+    vertex = "(" + x + "," + y + ")";//setting up the vertex 
+    meshold1 = vertex;//setting vertex
+    meshold2 = AOS; //setting aos 
+    results();//calling mssage board 
+}
+//vertex and AOS flowchart END 
